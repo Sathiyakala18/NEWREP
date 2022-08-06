b9f1b18e1d28587921f9a3bab638bc919961672a
Sathiyakala18 <105903238+Sathiyakala18@users.noreply.github.com>
2022-08-06 22:33:44 +0530

    Updated
    New code updated

--- GitHub.java
+++ GitHub.java
@@ -4,6 +4,13 @@ public class GitHub {
 	
 	public static void main(String[] args) {
 		
-	}
+		int a=10;
+		if( i%2==0);
+		}
 
+system.out.println(a+ "is a odd number");
+}
+else{
+system.out.println(a+ "is a even number");
+}
 }
